2c01c80f71fb363be8ee3d74b96c9542b214d35e
hobit22 <hobit22@naver.com>
2024-01-11 00:10:53 +0900

    [ADD] test api 추가

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
 @app.get("/")
-def test():
+def hello():
     return 'hello'
+
+
+@app.get('/test')
+def html_test():
+    return FileResponse('index.html')
+
+class Model(BaseModel):
+    name: str
+    phone: int
+
+@app.post("/send")
+def post_test(data: Model):
+    print(data)
+    return 'post success'
