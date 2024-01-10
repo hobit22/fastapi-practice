15b337a699e277a6d6c9adec1511cb3ad9e4d04a
hobit22 <hobit22@naver.com>
2024-01-11 00:48:31 +0900

    [ADD] db api 추가

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI
-
-app = FastAPI()
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
+import crud, models, schemas
+from database import SessionLocal, engine
+
+# models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 def hello():
     return 'hello'
-
 
 @app.get('/test')
 def html_test():
@@ -22,3 +36,18 @@ class Model(BaseModel):
 def post_test(data: Model):
     print(data)
     return 'post success'
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
